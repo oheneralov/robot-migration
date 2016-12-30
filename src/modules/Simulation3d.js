db0a440c15dabffa85dac1d6da633fcd24b7f92f
Alex <oheneralov@gmail.com>
2016-12-30 15:42:11 +0000

    added routes

---
+++ src/modules/Simulation3d.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Simulation3d extends React.Component {
+
+    render(){
+        return (
+            <div>3d simulation</div>
+        );
+    }
+}
+
+export default Simulation3d;
---
+++ src/modules/Simulation3d.js~
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Simulation2d extends React.Component {
+
+    render(){
+        return (
+            <div>2d simulation</div>
+        );
+    }
+}
+
+export default Simulation2d;
