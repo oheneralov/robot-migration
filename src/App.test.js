17978880c88dc8266184bce84dc9def449de3945
Alex <oheneralov@gmail.com>
2017-02-14 15:50:28 +0000

    added unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Simulation2d from './modules/Simulation2d';
+import Simulation3d from './modules/Simulation3d';
 import ReactTestUtils from 'react-addons-test-utils';
 
+/*
 it('renders Simulation2d without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Simulation2d />, div);
 });
 
+*/
 it ('checks simulation2d output', () => {
     const renderer = ReactTestUtils.createRenderer();
     renderer.render(<Simulation2d />);
     const result = renderer.getRenderOutput();
     expect(result.type).toBe('div');
 });
+
+it ('checks simulation3d output', () => {
+    const renderer = ReactTestUtils.createRenderer();
+    renderer.render(<Simulation3d />);
+    const result = renderer.getRenderOutput();
+    expect(result.type).toBe('div');
+});
+
+
